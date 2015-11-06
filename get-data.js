fc7576843f52e9fd88b7b401d0938211786acc25
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-11-06 13:32:24 +0500

    Make the code prettier

--- get-data.js
+++ get-data.js
@@ -1,37 +1,47 @@
 var request = require("request");
-module.exports.orderBooks =function () {
-        var url = "https://api.blinktrade.com/api/v1/PKR/orderbook"
-        request({
-            url: url,
-            json: true
-        }, function (error, response, body) {
 
-            if (!error && response.statusCode === 200) {
-                console.log(body) // Print the json response
-            }
-        })
+var api = {};
+
+// get orderbook
+api.orderbook = function() {
+  var url = "https://api.blinktrade.com/api/v1/PKR/orderbook";
+  request({
+    url: url,
+    json: true
+  }, function(error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+      console.log(body); // Print the json response
     }
-module.exports.ticker =function(){
-      var url = "https://api.blinktrade.com/api/v1/PKR/ticker"
-      request({
-        url: url,
-        json: true
-      }, function (error, response, body) {
+  });
+};
 
-      if (!error && response.statusCode === 200) {
-        console.log(body) // Print the json response
-       }
-    })
-  }
-module.exports.trades= function(){
-       var url = "https://api.blinktrade.com/api/v1/PKR/trades"
-       request({
-            url: url,
-            json: true
-         }, function (error, response, body) {
+// get ticker
+api.ticker = function() {
+  var url = "https://api.blinktrade.com/api/v1/PKR/ticker";
+  request({
+    url: url,
+    json: true
+  }, function(error, response, body) {
 
-          if (!error && response.statusCode === 200) {
-            console.log(body) // Print the json response
-           }
-      })
-}
+    if (!error && response.statusCode === 200) {
+      console.log(body); // Print the json response
+    }
+  });
+};
+
+// get last 24 trades
+api.trades = function() {
+  var url = "https://api.blinktrade.com/api/v1/PKR/trades";
+  request({
+    url: url,
+    json: true
+  }, function(error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+      console.log(body); // Print the json response
+    }
+  });
+};
+
+module.exports = api;
