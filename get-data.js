7bfa21c0c5d2b67108fbf6a9389f4c6025989290
Amin Shah Gilani <aminshahgilani@gmail.com>
2015-11-06 13:44:14 +0500

    add callback functionality

--- get-data.js
+++ get-data.js
@@ -3,45 +3,37 @@ var request = require("request");
 var api = {};
 
 // get orderbook
-api.orderbook = function() {
+publicApi.orderbook = function(callback) {
   var url = "https://api.blinktrade.com/api/v1/PKR/orderbook";
   request({
     url: url,
     json: true
   }, function(error, response, body) {
-
-    if (!error && response.statusCode === 200) {
-      console.log(body); // Print the json response
-    }
+    callback(error, body);
   });
 };
 
+
 // get ticker
-api.ticker = function() {
+publicApi.ticker = function() {
   var url = "https://api.blinktrade.com/api/v1/PKR/ticker";
   request({
     url: url,
     json: true
   }, function(error, response, body) {
-
-    if (!error && response.statusCode === 200) {
-      console.log(body); // Print the json response
-    }
+    callback(error, body);
   });
 };
 
 // get last 24 trades
-api.trades = function() {
+publicApi.trades = function() {
   var url = "https://api.blinktrade.com/api/v1/PKR/trades";
   request({
     url: url,
     json: true
   }, function(error, response, body) {
-
-    if (!error && response.statusCode === 200) {
-      console.log(body); // Print the json response
-    }
+    callback(error, body);
   });
 };
 
-module.exports = api;
+module.exports = publicApi;
